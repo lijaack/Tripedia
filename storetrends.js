abc11085ab8074f3da2edb52d42da1b2343e87a4
Isabel <iarcones@gmail.com>
2018-11-29 20:17:13 -0800

    connected index.html with the js for update info in DB,

--- storetrends.js
+++ storetrends.js
@@ -11,40 +11,42 @@
 
   var database = firebase.database();
 
-//   var destionationsRef = dbRef.ref('destinations');
 
-$("#xxxxxxxxxx").on("click", function (event) {
+/// this function will update the info of the destinations search, it will be used to calculate trends (we will store destination city, counter and timestamp added and timestamp updated, just in case we want to play with trends by year, month, etc...)
+$("#run-submit").on("click", function (event) {
     // Prevent form from submitting
     event.preventDefault();
 
     $("#error-text").text("");
 
     // Get the input values
-    var zzzzzzz = $("#yyyyyyy").val().trim();
-
+    var destination = $("#toDestination").val().trim();
+    
     // control if the input fields are empties
-    if (zzzz === "") {
+    if (destination === "") {
         $("#error-text").text("<--- All fields are mandatory, please complete the form --->");
         return;
     }
    
-
+    ///// database search city, get the counter add 1 to the counter and update info or create it
+    /// if the city exist update the counter and the dateUpdated
     database.ref().push({
-        cityorigin: cityorigin,
-        citydestination: citydestination,
-        counter: counter,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+        citydestination: destination,
+        counter: counter + 1,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP,
+        dateUpdated: firebase.database.ServerValue.TIMESTAMP
     });
 
 });
 
-database.ref().on("child_added", function (snapshot) {
 
-    var zzzzzz = snapshot.val().zzzzzzzz;
-    
+function updatetrends() {
+  /// this function wwill update the top trends destination of the year 
+  /// it will get all the city from the DB sortedby counter and it will send to the box the top 10
 
- 
+    // i think we can read orderby and limit to 10 the results (pending)
 
-}, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-});
+ };
+
+
+
